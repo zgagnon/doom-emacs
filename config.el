9d19278e02bd329dd3adf356bd6ae8a4e6934b52
Zoe Gagnon <zoe@zgagnon.com>
2025-02-28 23:54:41 -0500

    Rebuild config in a way that helps make it easier to troubleshoot

---
+++ config.el
@@ -0,0 +1,208 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 18 :weight 'medium)
+     doom-variable-pitch-font (font-spec :family "FiraCode Nerd Font" :size 18)
+      doom-symbol-font (font-spec :family "FiraCode Nerd Font" :size 18)
+      doom-serif-font (font-spec :family "FiraCode Nerd Font" :size 18))
+
+(setq org-directory "~/org/")
+
+
+(custom-set-faces!
+  `(org-level-1 :family "Luminari" :height 400)
+  `(org-level-2 :family "Cochin" :height 300)
+  `(org-level-3 :family "Rockwell" :height 200)
+  `(org-level-4 :family "Rockwell" :height 150)
+  `(org-level-5 :family "Rockwell" :height 150)
+  `(org-level-6 :family "Rockwell" :height 150)
+                )
+
+(setq display-line-numbers-type t)
+(setq-default tab-width 2)
+(global-visual-line-mode 1)
+(if (display-graphic-p)
+    (progn
+      (setq initial-frame-alist
+            '((tool-bar-lines . 0)
+              (width . 200)
+              (height . 400)
+              )
+            )
+      )
+  )
+
+(add-function :after after-focus-change-function
+  (lambda ()
+    (save-some-buffers t)))
+
+(after! persp-mode
+  ;; alternative, non-fancy version which only centers the output of +workspace--tabline
+  (defun workspaces-formatted ()
+    (+doom-dashboard--center (frame-width) (+workspace--tabline)))
+
+  (defun hy/invisible-current-workspace ()
+    "The tab bar doesn't update when only faces change (i.e. the
+current workspace), so we invisibly print the current workspace
+name as well to trigger updates"
+    (propertize (safe-persp-name (get-current-persp)) 'invisible t))
+
+  (customize-set-variable 'tab-bar-format '(workspaces-formatted tab-bar-format-align-right hy/invisible-current-workspace))
+
+  ;; don't show current workspaces when we switch, since we always see them
+  (advice-add #'+workspace/display :override #'ignore)
+  ;; same for renaming and deleting (and saving, but oh well)
+  (advice-add #'+workspace-message :override #'ignore))
+
+;; need to run this later for it to not break frame size for some reason
+(run-at-time nil nil (cmd! (tab-bar-mode +1)))
+
+(custom-set-faces!
+  '(+workspace-tab-face :inherit default :family "Jost" :height 135)
+  '(+workspace-tab-selected-face :inherit (highlight +workspace-tab-face)))
+
+(after! persp-mode
+  (defun workspaces-formatted ()
+    ;; fancy version as in screenshot
+    (+doom-dashboard--center (frame-width)
+        (let ((names (or persp-names-cache nil))
+                (current-name (safe-persp-name (get-current-persp))))
+        (mapconcat
+        #'identity
+        (cl-loop for name in names
+                for i to (length names)
+                collect
+                (concat (propertize (format " %d" (1+ i)) 'face
+                        `(:inherit ,(if (equal current-name name)
+                                        '+workspace-tab-selected-face
+                                        '+workspace-tab-face)
+                        :weight bold))
+                (propertize (format " %s " name) 'face
+                        (if (equal current-name name)
+                                '+workspace-tab-selected-face
+                        '+workspace-tab-face))))
+        " "))))
+;; other persp-mode related configuration
+)
+
+(setq auth-sources '((:source "~/.authinfo")))
+
+(use-package! gptel)
+
+(after! gptel
+  (map! :leader
+        (:prefix ("r" . "robot chat")
+         :desc  "Send the contents of the buffer up to point" "s"#'gptel-send
+         :desc  "Open a new chat buffer" "o" #'gptel
+         :desc "Open gptel menu" "m"  #'gptel-menu
+         :desc  "Add a region or buffer to the chat context" "a" #'gptel-add
+         :desc  "Add a file to the chat context" "f" #'gptel-add-file
+         :desc "Rewrite selected region" "r" #'gptel-rewrite
+         :desc "Limit to org mode topic" "t"  #'gptel-org-set-topic)))
+
+(use-package aider
+  :config
+  (setq aider-args '("--model" "gpt-4o-mini")))
+
+(setq user-full-name "Zoe Gagnon"
+      user-mail-address "zoe@zgagnon.com")
+
+(setq read-process-output-max (* 1024 1024))
+(setq gc-cons-threshold 100000000)
+(setq lsp-idle-delay 0.5)
+
+(use-package! treesit-auto
+  :custom
+  (treesit-auto-install 'prompt)
+  :config
+  (treesit-auto-add-to-auto-mode-alist 'all)
+  (global-treesit-auto-mode))
+
+(after! elixir-ts-mode
+  (map! :map elixir-ts-mode-map
+        :leader
+        (:prefix ("m t" . "Exunit tests")
+         :desc "Verify single ExUnit test" "s" #'exunit-verify-single
+         :desc "Verify all ExUnit tests" "a" #'exunit-verify-all
+         :desc "Verify all tests in the umbrella project" "u" #'exunit-verify-all-in-umbrella
+         :desc "Run all tests in this buffer" "v" #'exunit-verify
+         :desc "Rerun the last test invocation" "r" #'exunit-rerun
+         :desc "Rerun the last test with debugging" "d" #'exunit-debug
+         :desc "Toggle between file and test" "t" #'exunit-toggle-file-and-test
+         )))
+
+(use-package! lsp-tailwindcss)
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq doom-theme 'doom-one)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type t)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
