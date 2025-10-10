a1d97363383d485045653cafb9cd4709401ab78a
Zoe Gagnon <zoe@zgagnon.com>
2025-10-10 14:49:10 +0000

    remove tree undo

--- init.el
+++ init.el
@@ -73,7 +73,7 @@ word-wrap         ; soft wrapping with language-aware indent
 electric          ; smarter, keyword-based electric-indent
 ;;eww               ; the internet is gross
 ( ibuffer +icons)           ; interactive buffer management
-( undo +tree)              ; persistent, smarter undo for your inevitable mistakes
+ undo              ; persistent, smarter undo for your inevitable mistakes
 vc                ; version-control and Emacs, sitting in atree
 
 :term
